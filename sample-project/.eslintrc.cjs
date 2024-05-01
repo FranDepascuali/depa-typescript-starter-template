e96e2df7d2521194ed0a8a24eaca60ca73ee2ebb
Francisco Depascuali <depa.developer@gmail.com>
2024-05-01 20:30:53 +0200

    update template

--- sample-project/.eslintrc.cjs
+++ sample-project/.eslintrc.cjs
@@ -17,12 +17,18 @@ module.exports = {
     node: true,
     jest: true,
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+    },
+  ],
   ignorePatterns: [
     '.eslintrc.cjs',
     'build/**/*.*',
     'dist/**/*.*',
     'node_modules',
     'vite.config.ts',
+    '.eslintrc.js',
   ],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
@@ -30,5 +36,7 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/switch-exhaustiveness-check': 'error',
+    '@typescript-eslint/array-type': 'off',
   },
 }
